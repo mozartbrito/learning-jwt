6d69ae125031f3b3acdfd49cee3561585296d1e3
Mozart Teixeira de Brito <mozart.brito@cnm.local>
2017-11-06 15:27:44 -0200

    JWT with libraty eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImp0aSI6IjRmMWcyM2ExMmFhIn0.eyJpc3MiOiJodHRwOlwvXC9leGFtcGxlLmNvbSIsImF1ZCI6Imh0dHA6XC9cL2V4YW1wbGUub3JnIiwianRpIjoiNGYxZzIzYTEyYWEiLCJpYXQiOjE1MDk5ODkwNzYsIm5iZiI6MTUwOTk4OTEzNiwiZXhwIjoxNTA5OTkyNjc2LCJ1aWQiOjF9.bSAt7rsokH8SO0flBAla4fQFRUz_Bca4be_RBt6Ssn0

--- index.php
+++ index.php
@@ -1,2 +1,19 @@
 <?php 
 
+require_once('vendor/autoload.php');
+use Lcobucci\JWT\Builder;
+use Lcobucci\JWT\Signer\Hmac\Sha256;
+
+$signer = new Sha256();
+
+$token = (new Builder())->setIssuer('http://example.com') // Configures the issuer (iss claim)
+                        ->setAudience('http://example.org') // Configures the audience (aud claim)
+                        ->setId('4f1g23a12aa', true) // Configures the id (jti claim), replicating as a header item
+                        ->setIssuedAt(time()) // Configures the time that the token was issue (iat claim)
+                        ->setNotBefore(time() + 60) // Configures the time that the token can be used (nbf claim)
+                        ->setExpiration(time() + 3600) // Configures the expiration time of the token (exp claim)
+                        ->set('uid', 1) // Configures a new claim, called "uid"
+                        ->sign($signer, 'testing') // creates a signature using "testing" as key
+                        ->getToken(); // Retrieves the generated token
+
+echo $token;
